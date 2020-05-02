686ab7f80d291008fa293cae4b8f099c361b6104
Aolad Hossain Anna <32567035+aolad-anna@users.noreply.github.com>
2020-05-02 06:00:38 +0600

    Add files via upload

---
+++ BBM.BLL/EditDetailsRepo.cs
@@ -0,0 +1,31 @@
+﻿using BBM.DAL;
+using BBM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBM.BLL
+{
+    public class EditDetailsRepo
+    {
+        EditDetailsData userToEdit = new EditDetailsData();
+        private int returnedAddressId = 0, isSuccessful = 0;
+
+        public int editUser(UserInfo userObj, AddressInfo addressObj)
+        {
+            returnedAddressId = userToEdit.editUserAddress(addressObj);
+
+            if(userObj.AddressId == returnedAddressId)
+            {
+                isSuccessful = userToEdit.editUserInfo(userObj);
+            }
+
+            if (isSuccessful > 0)
+                return isSuccessful;
+            else
+                return 0;
+        }
+    }
+}
